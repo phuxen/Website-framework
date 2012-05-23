40b59c5f8e5c176311158a22a37074aefbb51cee
Paul Huxen <phuxen@gmail.com>
2012-05-23 22:16:40 +0100

    First push
    Pushed it real good

---
+++ assets/js/elements.js
@@ -0,0 +1,32 @@
+document.createElement("article");  
+document.createElement("footer");  
+document.createElement("header");  
+document.createElement("hgroup");  
+document.createElement("nav");
+
+$(function () {
+    var tabContainers = $('.tabContent');
+    $('ul.sideNav a').click(function () {
+        tabContainers.hide().filter(this.hash).show();
+        
+        $('ul.sideNav a').removeClass('selected');
+        $(this).addClass('selected');
+        
+        return false;
+    }).filter(':first').click();
+    
+    $('.faceInner').append($.gravatar('phuxen@gmail.com', {size: 250}));
+    
+    //603015
+    
+    /*$(".instagram").instagram({
+    	show : 		5,
+    	userId:		603015,
+    	clientId: 	'9edb71a8e03e4fcaa0639f09d725d9bd'
+	});*/
+	
+	$(".instagram").instagram('getStream', {
+		count: 6,
+		user: 603015
+	});
+});
